4afa68a97f9d7ab9cdb83fe96ca1a430fb3229d2
taiki.ogasawara <taiki.ogasawara@gmodecorp.com>
2015-10-06 09:35:29 +0900

    デブキャラ追加

--- colosseum/CharFat.swift
+++ colosseum/CharFat.swift
@@ -1,35 +1,35 @@
 import SpriteKit
 
-class CharHardbodyFemale
+class CharFat
 {
     var data: CharBase;
     
     static func getName() -> String {
-        return CharManager.CharNames.hardbodyFemale.rawValue;
+        return CharManager.CharNames.fat.rawValue;
     }
     static func getImageName() -> String {
         return "TestChar2";
     }
     static func getDisplayName() -> String {
-        return "マリー";
+        return "ファド";
     }
-
+    
     init() {
         
-        data = CharBase(imageNamed: CharHardbodyFemale.getImageName());
-        data.displayName = CharHardbodyFemale.getDisplayName();
-        data.name = CharHardbodyFemale.getName();
+        data = CharBase(imageNamed: CharFat.getImageName());
+        data.displayName = CharFat.getDisplayName();
+        data.name = CharFat.getName();
         data.gaugeInit(CGSizeMake(data.size.width, 5), direction: Gauge.Direction.horizontal, zPos: 0);
         
-        // 攻撃特化型
+        // 防御特化型
         
         // ステータス設定
-        data.statusInit(hp: 3000, atk: 300, def: 100, hit: 90, avd: 20, add_atk: 0);
+        data.statusInit(hp: 4000, atk: 200, def: 200, hit: 90, avd: 20, add_atk: 0);
         
         // ゲージ設定
         data.gaugeLangeth = 200.0;
         data.gaugeAcceleration = 30.0;
-
+        
         // 行動設定
         
         // 行動0 : 行動なし(固定)
@@ -39,89 +39,80 @@ class CharHardbodyFemale
         
         
         // 行動1
-        // 現在攻撃力の1.25倍と0.75倍の2回攻撃
+        // 現在攻撃力の1.00倍の攻撃
         // コスト2
         var atk_1 = CharBtlAction.Atk();
-        atk_1.atkPower = 125.0;
-        var atk_2 = CharBtlAction.Atk();
-        atk_2.atkPower = 75.0;
+        atk_1.atkPower = 100.0;
         var act_1 = CharBase.Action();
         act_1.action.type = CharBtlAction.ActType.atk;
         act_1.action.atk.append(atk_1);
-        act_1.action.atk.append(atk_2);
         act_1.cost = act_1.action.atkCost;
-        act_1.name = "強連撃";
+        act_1.name = "攻撃";
         data.actions.append(act_1);
         
         
         // 行動2
-        // 現在攻撃力の0.5倍で1回防御
+        // 現在攻撃力の2倍で2回防御
         // 3倍でカウンター
         // コスト2
         var def_1 = CharBtlAction.Def();
         def_1.seedType = CharBtlAction.SeedType.atkNow;
-        def_1.defPower = 0.3;
-        def_1.defCount = 1;
+        def_1.defPower = 3.0;
+        def_1.defCount = 2;
         def_1.defCounterAttack = 300.0;
         var act_2 = CharBase.Action();
         act_2.action.type = CharBtlAction.ActType.def;
         act_2.action.def.append(def_1);
         act_2.cost = act_2.action.defCost;
-        act_2.name = "反撃の構え";
+        act_2.name = "プレッシャー";
         data.actions.append(act_2);
         
         
         // 行動3
-        // 基本攻撃力の0.5倍で相手攻撃力を減少
+        // 相手命中を10減少
         // 2ターン
         var jam_1 = CharBtlAction.Jam();
-        jam_1.type = CharBtlAction.JamType.weakenAtk;
+        jam_1.type = CharBtlAction.JamType.weakenHit;
         jam_1.seedType = CharBtlAction.SeedType.atkBase;
-        jam_1.power = 50.0;
-        jam_1.turn = 3;
-        jam_1.addDamage = 5.0;
+        jam_1.power = 10.0;
+        jam_1.turn = 2;
+        jam_1.addDamage = 6.0;
         var act_3 = CharBase.Action();
         act_3.action.type = CharBtlAction.ActType.jam;
         act_3.action.jam.append(jam_1);
         act_3.cost = act_3.action.jamCost;
-        act_3.name = "武器破壊";
+        act_3.name = "油飛ばし";
         data.actions.append(act_3);
         
         
         // 行動4
-        // 現在攻撃分の0.5倍の攻撃力と防御力が上がる
+        // 基本攻撃分の0.75倍の防御力が上がる
         // 2ターン
         var enh_1 = CharBtlAction.Enh();
-        enh_1.type = CharBtlAction.EnhType.atk;
+        enh_1.type = CharBtlAction.EnhType.def;
         enh_1.seedType = CharBtlAction.SeedType.atkBase;
-        enh_1.power = 50.0;
+        enh_1.power = 75.0;
         enh_1.turn = 2;
-        var enh_2 = CharBtlAction.Enh();
-        enh_2.type = CharBtlAction.EnhType.def;
-        enh_2.seedType = CharBtlAction.SeedType.atkBase;
-        enh_2.power = 50.0;
-        enh_2.turn = 2;
         var act_4 = CharBase.Action();
         act_4.action.type = CharBtlAction.ActType.enh;
         act_4.action.enh.append(enh_1);
-        act_4.action.enh.append(enh_2);
         act_4.cost = act_4.action.enhCost;
-        act_4.name = "ビルドアップ";
+        act_4.name = "膨らむ";
         data.actions.append(act_4);
         
         
         // 特技
-        // 基本攻撃力の3.0倍で1回攻撃
+        // 防御種別で攻撃力2.0倍で1回攻撃
         var atk_3 = CharBtlAction.Atk();
-        atk_3.atkPower = 300.0;
+        atk_3.atkPower = 200.0;
         var skl_1 = CharBtlAction.Skl();
         skl_1.type = CharBtlAction.ActType.atk;
         skl_1.atk.append(atk_3);
         
         var act_5 = CharBase.Action();
         act_5.type = CharBase.ActionType.skl;
-        act_5.name = "無慈悲強撃";
-        act_5.action.type = CharBtlAction.ActType.atk;
+        act_5.name = "押しつぶす";
+        act_5.action.type = CharBtlAction.ActType.def;
         act_5.action.skl.append(skl_1);
         act_5.cost = act_5.action.sklCost;
         data.actions.append(act_5);
@@ -129,21 +120,22 @@ class CharHardbodyFemale
         
         
         data.speech_battlePre = [
-            "私と戦うのはおすすめしないわ",
-            "止めもしないけれど"
+            "どうせ俺はデブだよ",
+            "だから彼女もできない"
         ];
         
         data.speech_battleStart = [
-            "私は強いわよ",
-            "力でねじ伏せてあげるわ"
+            "俺なんてどうせダメ",
+            "だってデブだもん"
         ];
         
         data.speech_battleEnd_win = [
-            "当然よね。私は強いもの"
+            "デブに負けるとかwww",
+            "デブ以下カワイソウwww"
         ];
         
         data.speech_battleEnd_lose = [
-            "もういや。しにたい"
+            "当然だよね"
         ];
     }
 }
--- colosseum/CharManager.swift
+++ colosseum/CharManager.swift
@@ -8,15 +8,71 @@ final class CharManager
     }
     
     enum CharNames: String {
-        case mob = "Mob"
         case hero = "Hero"
+        case mob = "Mob"
         case hardbodyFemale = "HardbodyFemale"
+        case fat = "Fat"
+        case poorHealth = "PoorHealth"
+        case ugly = "Ugly"
+        case tease = "Tease"
+        case toxic = "Toxic"
+        case elite = "Elite"
+        case majority = "Majority"
     }
     static func getChar(name: String) -> CharBase {
         switch name {
-        case CharNames.mob.rawValue:              return CharMob().data;
-        case CharNames.hardbodyFemale.rawValue:   return CharHardbodyFemale().data;
+        case CharNames.mob.rawValue:                return CharMob().data;
+        case CharNames.hardbodyFemale.rawValue:     return CharHardbodyFemale().data;
+        case CharNames.fat.rawValue:                return CharFat().data;
         default: return CharHero().data;
         }
     }
+    
+    
+    /*
+    　主人公
+    　　特徴がない。全てにおいて平均的。(ユーザの影となる扱いの為)
+    　
+    　筋肉女「マリー」
+    　　筋肉がコンプレックス。
+    　　コンプレックスと向き合うためにコロシアムに参加。
+    　　攻撃が得意。
+    　
+    　デブ男「ファド」
+    　　デブでモテないのがコンプレックス。
+    　　とても卑屈で話しているとイライラする。
+    　　防御が得意。
+    　
+    　病弱賢人のおっさん「パーヘル」
+    　　病弱がコンプレックス。
+    　　頭が良いので心の中では常に人を見下している。
+    　　妨害が得意。
+    　
+    　ブサイク男「ウーグ」
+    　　ブサイクがコンプレックス。
+    　　あまりにもモテない反動で女性を見下す態度をとってしまう。
+    　　ある宗教団体に所属しており、強くなりモテると噂の怪しい薬に手を出している。
+    　　強化が得意。
+    　
+    　いじめられた男「ティーズ」
+    　　義務教育時期にいじめられたため他人が怖い。
+    　　その経験から人が苦しんでいるのを見るのが快感となった。
+    　　攻撃と防御がやや得意。
+    　　
+    　親切美女「トクスィ」
+    　　幼い頃に毒親に身勝手な愛情を受けたため歪んだ性格をしている。
+    　　普段は物腰柔らかで美しい人だが優しさが拒否されるとヒステリーをおこす。
+    　　強化と妨害がやや得意。
+    　　
+    　エリート男「ユーリード」
+    　　エリート一家にて一人だけ冴えなかったことがコンプレックス。
+    　　親も扱いに困りあまり愛されなかったことで態度が大きい寂しがり屋となった。
+    　　得意不得意はない。
+    　　
+    　マジョリティ女「マーショリー」
+    　　自分がなく常に多数派の意見に流される。
+    　　多数派が正しいと思い込んでおり少数派の意見にはとても攻撃的になる。
+    　　妨害が得意、強化がやや得意、防御がやや苦手。
+
+    */
 }
